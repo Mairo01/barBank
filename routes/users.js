558322f07b4cec040fc705687c0ded3209af7301
Mairo01 <mairo.planken@khk.ee>
2021-09-23 08:47:49 -0400

    As a user I can see my account details [Delivers #179685169]

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const User = require('../models/User')
 const Account = require('../models/Account')
+const {verifyToken} = require("../middlewares");
 
 router.post('/', async function(req, res) {
     if (req.body.password === undefined || req.body.password.length < 9) {
@@ -24,6 +25,22 @@ router.post('/', async function(req, res) {
         return res.status(500).send({error: e.message})
     }
     return res.status(201).send('')
+})
+
+router.get('/current', verifyToken, async function(req, res) {
+    const user = await User.findOne({_id: req.userId})
+    const account = await Account.findOne({userId: req.userId})
+
+    return res.status(200).send({
+            username: user.username,
+            name: user.name,
+            account: {
+                name: account.name,
+                number: account.number,
+                balance: account.balance,
+                currency: account.currency
+            }
+    })
 })
 
 module.exports = router
